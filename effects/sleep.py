e1246a1953cd898cd9b109e7aed83afb098cbafd
d3rbastl3r <igor.martens.83@gmail.com>
2022-12-04 21:08:51 +0100

    Remove unwanted constant

--- effects/sleep.py
+++ effects/sleep.py
@@ -7,7 +7,6 @@ import click
 from rpi_ws281x import PixelStrip, Color
 
 # LED strip default configuration:
-#LED_COUNT = 50       # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
